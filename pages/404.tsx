10eb24b01e5ecc46f857d9478608487194848057
Sasa Marjancic <s.marjancic@gmail.com>
2025-05-25 08:10:26 +0200

    Fix apostrophe in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,9 +5,9 @@ export default function Custom404() {
 			<h1 className="text-5xl font-bold mb-4 text-gray-800">
 				404 – Page Not Found
 			</h1>
-			<p className="text-gray-600 mb-6">
-				Sorry, we couldn’t find that grant or page.
-			</p>
+                        <p className="text-gray-600 mb-6">
+                                Sorry, we couldn't find that grant or page.
+                        </p>
 			<a
 				href="/"
 				className="text-blue-600 underline text-sm hover:text-blue-800"
